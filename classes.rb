2af7446987b08b3536e228a68422d1dab4c47e22
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-30 19:20:51 -0400

    incrementally building

---
+++ classes.rb
@@ -0,0 +1,49 @@
+# def question
+  #   num1 = rand(1..20)
+  #   num2 = rand(1..20)
+  #   puts "What is #{num1} + #{num2}?"
+  # end
+  
+  # puts question
+  
+  # def answer_validation
+  #   user_input = gets.chomp.to_i
+  #   sum = num1 + num2
+  #   validation = sum == user_input ? @player[:lives] : @player[:lives] - 1
+  # end
+  
+  class Game
+    def initialize
+      player1
+      player2
+      question
+      swap_player
+    end
+  end
+  
+  class PlayerClass
+    attr_accessor :name
+    attr_writer :lives
+    def initialize(name, lives)
+      @name = name
+      @lives = 3
+    end
+  end
+  
+  class QuestionClass
+    attr_accessor :user_input
+    def initialize
+      @num1 = rand(1..20)
+      @num2 = rand(1..20)
+      puts "What is #{@num1} + #{@num2}?"
+      @user_input = gets.chomp.to_i
+      
+    end
+  
+    def answer_validation
+      sum = @num1 + @num2
+      puts "You entered #{user_input} and the correct answer is #{sum}"
+      validation = sum == user_input ? "that's right !"   @player[:lives] : "that's wrong!"  @player[:lives] - 1
+      puts validation
+    end
+  end
--- main.rb
+++ main.rb
@@ -1,48 +1,6 @@
 require 'pp'
 require './classes'
 
-def question
-  num1 = rand(1..20)
-  num2 = rand(1..20)
-  puts "What is #{num1} + #{num2}?"
-end
+question = QuestionClass.new
 
-puts question
-
-def answer_validation
-  user_input = gets.chomp.to_i
-  sum = num1 + num2
-  validation = sum == user_input ? player[:lives] : player[:lives] - 1
-end
-
-class Game
-  def initialize
-    player1
-    player2
-    question
-    swap_player
-  end
-end
-
-class PlayerClass
-  attr_accessor :name
-  attr_writer :lives
-  def initialize(name, lives)
-    @name = name
-    @lives = 3
-  end
-end
-
-class QuestionClass
-  
-  def initialize(num1, num2)
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
-    puts "What is #{num1} + #{num2}?"
-  end
-
-  def answer_validation
-    user_input = gets.chomp.to_i
-    sum = num1 + num2
-    validation = sum == user_input ? player[:lives] : player[:lives] - 1
-  end
+question.answer_validation
